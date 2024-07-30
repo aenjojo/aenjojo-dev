9f32d8cc43e6de77de1110fed69ef8b39036552e
aenjojo <josuafernando999@gmail.com>
2024-07-30 22:02:05 +0700

    chore: create default page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="w-screen h-screen grid place-content-center bg-gradient-to-br from-blue-400 to-pink-400">
+  <h1 class="text-6xl lg:text-8xl font-bold bg-gradient-to-r from-pink-600 to-blue-600 text-transparent bg-clip-text p-4">
+    AenJojo
+  </h1>
+</div>
