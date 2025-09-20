cde62504dba74cd506c08b97ff6fea809868f5f4
aenjojo <josuafernando999@gmail.com>
2025-09-20 18:11:25 +0700

    feat: add `robots.txt` handler

---
+++ src/routes/link/robots.txt/+server.ts
@@ -0,0 +1,13 @@
+export function GET() {
+  const robots = `
+    User-agent: *
+    Disallow: /
+  `.replaceAll(/ {2,}/g, '');
+
+  return new Response(robots, {
+    headers: {
+      'content-type': 'text/plain; charset=utf-8',
+      'cache-control': 'public, max-age=3600',
+    },
+  });
+}
