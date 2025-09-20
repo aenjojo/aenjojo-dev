4d44be3bc7e92ec59c409ed925ba1a58a20998ca
aenjojo <josuafernando999@gmail.com>
2025-09-20 18:13:21 +0700

    feat: add constant keys

--- src/lib/data/constant.server.ts
+++ src/lib/data/constant.server.ts
@@ -1,5 +1,14 @@
 export const isProductionMode = process.env.NODE_ENV === 'production';
 
 export const keys = {
-  sesionToken: 'ssit',
+  cookie: {
+    sesionToken: 'ssit',
+  },
+  cache: {
+    about: 'resume:about',
+    experience: 'resume:experience',
+    education: 'resume:education',
+    skill: 'resume:skill',
+    project: 'resume:project',
+  },
 };
