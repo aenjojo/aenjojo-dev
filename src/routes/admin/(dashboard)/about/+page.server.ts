cc8b2fe9816154d42f59f3fdddf5a42bdadb27f4
aenjojo <josuafernando999@gmail.com>
2025-09-23 18:15:41 +0700

    feat: add action service for about page

--- src/routes/admin/(dashboard)/about/+page.server.ts
+++ src/routes/admin/(dashboard)/about/+page.server.ts
@@ -1,7 +1,53 @@
+import { db } from '$lib/services/db.server';
+import { fail } from '@sveltejs/kit';
 import { checkAccess } from '../check-access.server';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ cookies }) => {
   await checkAccess(cookies);
-  return;
+
+  const data = await db.about.findFirst({
+    select: {
+      name: true,
+      role: true,
+      summary: true,
+    },
+    where: { id: '1' },
+  });
+
+  if (!data) {
+    return {
+      name: '',
+      role: '',
+      summary: '',
+    };
+  }
+
+  return data;
 };
+
+export const actions = {
+  default: async ({ cookies, request }) => {
+    await checkAccess(cookies);
+
+    const form = await request.formData();
+    const name = form.get('name');
+    const role = form.get('role');
+    const summary = form.get('summary');
+
+    if (!name) {
+      return fail(400, { name, missing: true });
+    }
+
+    await db.about.update({
+      data: {
+        name: name.toString(),
+        role: role?.toString() || '',
+        summary: summary?.toString() || '',
+      },
+      where: { id: '1' },
+    });
+
+    return { success: true };
+  },
+} satisfies Actions;
