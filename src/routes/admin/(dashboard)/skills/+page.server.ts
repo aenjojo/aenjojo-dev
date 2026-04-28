02b5340a41c1df5874f96162757a1605a9a4f49e
aenjojo <josuafernando999@gmail.com>
2026-04-28 18:10:58 +0700

    feat: add skill admin page

--- src/routes/admin/(dashboard)/skills/+page.server.ts
+++ src/routes/admin/(dashboard)/skills/+page.server.ts
@@ -1,3 +1,4 @@
+import { fail } from '@sveltejs/kit';
 import { db } from '$lib/services/db.server';
 import { checkAccess } from '../check-access.server';
 import type { Actions, PageServerLoad } from './$types';
@@ -12,17 +13,47 @@ export const load: PageServerLoad = async ({ cookies }) => {
     },
   });
 
-  return skills;
+  return { skills };
 };
 
 export const actions = {
-  create: async () => {
-    //
+  create: async ({ cookies, request }) => {
+    await checkAccess(cookies);
+
+    const form = await request.formData();
+    let name = form.get('name');
+
+    if (!name) {
+      return fail(400, { name, missing: true });
+    }
+
+    name = name.toString();
+    const id = name.toLowerCase().replaceAll(/ +/g, '-');
+
+    await db.skill.upsert({
+      create: { id, name },
+      update: { name },
+      where: { id },
+    });
+
+    return { success: true };
   },
-  update: async () => {
-    //
-  },
-  delete: async () => {
-    //
+  delete: async ({ cookies, request }) => {
+    await checkAccess(cookies);
+
+    const form = await request.formData();
+    const id = form.get('id');
+
+    if (!id) {
+      return fail(400, { id, missing: true });
+    }
+
+    await db.skill.delete({
+      where: {
+        id: id.toString().toLowerCase().replaceAll(/ +/g, '-'),
+      },
+    });
+
+    return { success: true };
   },
 } satisfies Actions;
--- src/routes/admin/(dashboard)/skills/+page.svelte
+++ src/routes/admin/(dashboard)/skills/+page.svelte
@@ -1,5 +1,62 @@
 <script lang="ts">
-  //
+  import { enhance } from "$app/forms";
+  import Input from "$lib/components/input.svelte";
+  import type { PageProps } from "./$types";
+
+  const { data }: PageProps = $props();
+  let saving = $state(false);
 </script>
 
-<h1>Skill</h1>
+<div class="p-4 bg-white space-y-4 w-full">
+  <h1 class="text-2xl">Skills</h1>
+  <div class="grid grid-cols-6">
+    <div
+      class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 col-span-4"
+    >
+      <table class="table table-sm">
+        <thead>
+          <tr>
+            <th></th>
+            <th>ID</th>
+            <th>Name</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each data.skills as skill, index}
+            <tr>
+              <td class="font-bold text-center">{index + 1}</td>
+              <td>{skill.id}</td>
+              <td>{skill.name}</td>
+              <td class="text-center">
+                <form method="POST" action="/admin/skills?/delete" use:enhance={({formData}) => {
+                  formData.set('id', skill.id);
+                }}>
+                  <button class="btn btn-sm btn-soft btn-error">Delete</button>
+                </form>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+
+    <form
+      class="p-4 space-y-4 w-full col-span-2"
+      method="POST"
+      action="/admin/skills?/create"
+      use:enhance={() => {
+        saving = true;
+        return async ({ update }) => {
+          saving = false;
+          await update();
+        };
+      }}
+    >
+      <div class="w-full max-w-xs sticky top-0">
+        <Input name="name" label="Skill name" type="text" value="" required />
+        <button class="btn btn-success">{saving ? "Adding" : "Add"}</button>
+      </div>
+    </form>
+  </div>
+</div>
