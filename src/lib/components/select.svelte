95c243889ded23caa04c6f04585d9df1d1f2ee12
aenjojo <josuafernando999@gmail.com>
2026-05-11 19:07:03 +0700

    feat: add selector element

---
+++ src/lib/components/select.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { HTMLSelectAttributes } from "svelte/elements";
+
+  interface Props extends HTMLSelectAttributes {
+    label: string;
+    note?: string;
+    options: string[];
+  }
+
+  const { label, note, required, options, ...props }: Props = $props();
+</script>
+
+<fieldset class="fieldset">
+  <legend class="fieldset-legend">
+    {label}{required ? "" : " (optional)"}
+  </legend>
+  {#if note}
+    <p class="label">{note}</p>
+  {/if}
+  <select class="select capitalize" {...props}>
+    <option disabled selected>Select {label.toLowerCase()}</option>
+    {#each options as option}
+      <option>{option}</option>
+    {/each}
+  </select>
+</fieldset>
