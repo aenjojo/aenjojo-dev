d396cb9830f2eb084e1254389e70f94b9d929ad4
aenjojo <josuafernando999@gmail.com>
2026-05-11 19:49:39 +0700

    chore: remove not required files

--- src/routes/admin/(dashboard)/skills/[id]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { checkAccess } from '../../check-access.server';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ cookies }) => {
-  await checkAccess(cookies);
-  return;
-};
